ae90d8770d50d946288cea2463feb3d8b3594fce
growthhuman <growthhuman@gmail.com>
2017-09-04 00:25:54 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kenta/imabayashi/qa_app/Const.java
@@ -0,0 +1,14 @@
+package jp.techacademy.kenta.imabayashi.qa_app;
+
+/**
+ * Created by kenta on 2017/09/02.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+
+}
---
+++ app/src/main/java/jp/techacademy/kenta/imabayashi/qa_app/QuestionSendActivity.java
@@ -0,0 +1,13 @@
+package jp.techacademy.kenta.imabayashi.qa_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionSendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_send);
+    }
+}
